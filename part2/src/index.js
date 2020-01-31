792bcacc6e8ee92e2fff76c13212e61f8039989a
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-01-31 12:35:03 +0530

    part2: 2.2 and 2.3
    part 2.3 (using reduce) was done in advance :)

--- part2/src/index.js
+++ part2/src/index.js
@@ -12,22 +12,19 @@ const Course = ({ course }) => {
     )
 }
 
-const Header = (props) =>
-    <h1>{props.course}</h1>
+const Header = ({ course }) =>
+    <h1>{course}</h1>
 
-const Part = (props) =>
-    <p>{props.part.name} {props.part.exercises}</p>
+const Part = ({ part }) =>
+    <p>{part.name} {part.exercises}</p>
 
-const Content = (props) =>
-    <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
-    </div>
+const Content = ({ parts }) =>
+    parts.map(part => <Part key={part.id} part={part} />)
 
-const Total = (props) =>
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises +
-        props.parts[2].exercises}</p>
+const Total = ({ parts }) => {
+    const total = parts.map(part => part.exercises).reduce((a, b) => a + b, 0)
+    return (<b>total of {total} exercises</b>)
+}
 
 const App = () => {
     const course = {
@@ -35,7 +32,8 @@ const App = () => {
         parts: [
             { name: 'Fundamentals of React', exercises: 10, id: 1 },
             { name: 'Using props to pass data', exercises: 7, id: 2 },
-            { name: 'State of a component', exercises: 14, id: 3 }
+            { name: 'State of a component', exercises: 14, id: 3 },
+            { name: 'Redux', exercises: 11, id: 4 },
         ]
     }
 
