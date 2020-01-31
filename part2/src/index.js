63c37d1832df2ca2abf66161239ffca708bc9140
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-01-31 17:35:31 +0530

    part2: 2.4

--- part2/src/index.js
+++ part2/src/index.js
@@ -12,6 +12,9 @@ const Course = ({ course }) => {
     )
 }
 
+const Courses = ({ courses }) =>
+    courses.map(course => <Course key={course.id} course={course} />)
+
 const Header = ({ course }) =>
     <h1>{course}</h1>
 
@@ -27,18 +30,27 @@ const Total = ({ parts }) => {
 }
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            { name: 'Fundamentals of React', exercises: 10, id: 1 },
-            { name: 'Using props to pass data', exercises: 7, id: 2 },
-            { name: 'State of a component', exercises: 14, id: 3 },
-            { name: 'Redux', exercises: 11, id: 4 },
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development', id: 1,
+            parts: [
+                { name: 'Fundamentals of React', exercises: 10, id: 1 },
+                { name: 'Using props to pass data', exercises: 7, id: 2 },
+                { name: 'State of a component', exercises: 14, id: 3 },
+                { name: 'Redux', exercises: 11, id: 4 },
+            ]
+        },
+        {
+            name: 'Node.js', id: 2,
+            parts: [
+                { name: 'Routing', exercises: 3, id: 1 },
+                { name: 'Middlewares', exercises: 7, id: 2 }
+            ]
+        }
+    ]
 
     return (
-        <Course course={course} />
+        <Courses courses={courses} />
     )
 }
 ReactDOM.render(<App />, document.getElementById('root'));
