a5a2a29a1163dbb38d6d0c57091e5a134581ff50
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-02-01 09:14:52 +0530

    part2: 2.12

---
+++ countries/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Filter = ({ country, setCountry }) =>
+  <p>find countries <input value={country}
+    onChange={event => setCountry(event.target.value)} /></p>
+
+const Country = ({ country }) =>
+  <div className="countryDetails">
+    <h1>{country.name}</h1>
+    <p>{country.capital}</p>
+    <p>population {country.population}</p>
+    <h2>languages</h2>
+    <ul>
+      {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+    </ul>
+    <img src={country.flag} alt={country.name} />
+  </div>
+
+const App = () => {
+  const [country, setCountry] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all")
+      .then(response => setCountries(response.data))
+  }, [])
+
+  const filteredCountries = countries
+    .filter(c => country !== '' && c.name.toLowerCase().startsWith(country.toLowerCase()))
+    .reduce((a, b) => a.concat(b), [])
+
+  if (filteredCountries.length > 10) {
+    // too many countries
+    return (
+      <div className="App">
+        <Filter country={country} setCountry={setCountry} />
+        <p>Too many matches, specify another filter</p>
+      </div>
+    )
+  } else if (filteredCountries.length === 1) {
+    // country details
+    return (
+      <div className="App">
+        <Filter country={country} setCountry={setCountry} />
+        <Country country={filteredCountries[0]} />
+      </div>
+    )
+  } else {
+    // list of countries
+    return (
+      <div className="App">
+        <Filter country={country} setCountry={setCountry} />
+        {filteredCountries.map(country => <p key={country.name}>{country.name}</p>)}
+      </div>
+    )
+  }
+}
+
+export default App;
