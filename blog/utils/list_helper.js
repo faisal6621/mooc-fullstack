88dfe2cccefd4476589d4d301b0c41ff95c33c32
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-03-01 12:15:04 +0530

    part4: 4.5

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -1,11 +1,17 @@
 const dummy = (blogs) => {
   // some code to be added later using blogs
-  console.log(blogs.length)
+  console.log('dummy blogs.length', blogs.length)
   return 1
 }
 
 const totalLikes = (blogs) => blogs.map((blog) => blog.likes)
   .reduce((sum, likes) => sum + likes, 0)
 
+const favoriteBlog = (blogs) => {
+  const favBlog = blogs.map((blog) => blog)
+    .reduce((favorite, current) => (current.likes > favorite.likes ? current : favorite),
+      { title: '', author: '', likes: 0 })
+  return favBlog
+}
 
-module.exports = { dummy, totalLikes }
+module.exports = { dummy, totalLikes, favoriteBlog }
