7457588b28fb6d303a2d49534e577bccd8e46bf2
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-03-01 11:25:03 +0530

    part4: 4.4

--- blog/utils/list_helper.js
+++ blog/utils/list_helper.js
@@ -1,6 +1,11 @@
 const dummy = (blogs) => {
   // some code to be added later using blogs
+  console.log(blogs.length)
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => blogs.map((blog) => blog.likes)
+  .reduce((sum, likes) => sum + likes, 0)
+
+
+module.exports = { dummy, totalLikes }
