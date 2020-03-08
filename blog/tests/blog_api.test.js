baa2ef4e6a3be56c9e70007943ef0a49461f7d4e
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-03-08 11:46:16 +0530

    part4: 4.10

--- blog/tests/blog_api.test.js
+++ blog/tests/blog_api.test.js
@@ -12,23 +12,50 @@ const blogs = [
     url: 'https://dev.to/faisal6621/minimum-configuration-for-log4j-20jf',
     likes: '2',
   },
+  {
+    title: 'Just another blog',
+    author: 'Mohammad Faisal',
+    url: '127.0.0.1',
+    likes: 1,
+  },
 ]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  const blog = new Blog(blogs[0])
-  await blog.save()
+  const promises = blogs.map((blog) => new Blog(blog))
+    .map((blog) => blog.save())
+
+  await Promise.all(promises)
 })
 
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
   expect(response.body.length).toBe(blogs.length)
 })
 
 test('each blog has id', async () => {
   const response = await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
   response.body.map((blog) => expect(blog.id).toBeDefined())
+})
+
+test('new blog has been added', async () => {
+  await api.post('/api/blogs')
+    .send({
+      title: 'Jest test blog', author: 'Jest', url: '127.0.0.1', likes: 1,
+    })
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map((blog) => blog.title)
+
+  expect(response.body.length).toBe(blogs.length + 1)
+  expect(titles).toContain('Jest test blog')
 })
 
 afterAll(() => mongoose.connection.close())
