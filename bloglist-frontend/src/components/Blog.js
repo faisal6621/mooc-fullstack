9af27282c60bbe50d2be28e430731a6185b449d6
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2020-04-05 22:34:43 +0530

    pickrt5: 5.7
    hide/show blog details

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,42 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} &rarr; <em>{blog.author}</em>
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const viewBlog = {
+    display: visible ? 'none' : ''
+  }
+
+  const hideBlog = {
+    display: visible ? '' : 'none'
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p>{blog.title}
+        <button onClick={toggleVisibility} style={viewBlog}>view</button>
+        <button onClick={toggleVisibility} style={hideBlog}>hide</button>
+      </p>
+      <p style={hideBlog}>
+        {blog.url}<br />
+        likes: {blog.likes} <button>like</button><br />
+        {blog.author}
+      </p>
+    </div>
+  )
+}
 
 export default Blog
